27503c0dbf53754ed7e92b87654121c792be8a66
Artiom Nazarchuk <sel_it@tut.by>
2022-12-28 16:33:46 +0300

    link to new text.

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_index)
+    path('', views.index),
+    path('info/', views.info),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse('<a href="info">Ссылка на новый текст</a>')
 
-def main_index(request):
-    return HttpResponse('Hello')
+def info(request):
+    return HttpResponse('<a href="/">Новый текст, ссылка на главную</a>')
 
