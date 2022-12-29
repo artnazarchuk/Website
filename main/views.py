ecdcc0cd4b74235fd10a435817076198b4ea53a3
Artiom Nazarchuk <sel_it@tut.by>
2022-12-29 14:34:20 +0300

    Adding a new template to a folder templates.

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<a href="info">Ссылка на новый текст</a>')
+    return render(request, 'home.html')
 
 def info(request):
     return HttpResponse('<a href="/">Новый текст, ссылка на главную</a>')
