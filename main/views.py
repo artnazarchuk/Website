dda746907053cd377b0181880b74bae8570a21dd
Artiom Nazarchuk <sel_it@tut.by>
2022-12-29 14:56:18 +0300

    Refactoring templates/home.html to templates/main/index.html

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'main/index.html')
 
 def info(request):
     return HttpResponse('<a href="/">Новый текст, ссылка на главную</a>')
