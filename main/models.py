2fedd4d0640493761feb9be44541961465b74daa
Artjom Nazarchuk <sel_it@tut.by>
2023-01-01 12:55:14 +0300

    Adding a class to models.py, using of Jinja.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Main_model(models.Model):
+    title = models.CharField('Title', max_length=50)
+    description = models.TextField('description')
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    image = models.FileField(upload_to='img/')
+
+    def __str__(self):
+        return self.title
