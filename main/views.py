0971a2eff88a0cc1669c00f9c65f944e02479b5e
Artiom Nazarchuk <sel_it@tut.by>
2023-01-05 16:17:41 +0300

    Adding create.html to template.

--- main/views.py
+++ main/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from .models import Main_model
 
 def index(request):
-    main_models = Main_model.objects.all()
+    main_models = Main_model.objects.order_by('id')
     return render(request, 'main/index.html', {'title': 'Главная страница', 'main_models': main_models})
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    return render(request, 'main/create.html')
+
 
